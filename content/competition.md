0962282e4995233010b61a073cca885398c313ea
arctic-hen7 <arctic.hen@pm.me>
2023-07-05 11:05:15 +1000

    feat: finalised static sections of website
    Fixed up all placeholder content and added images.

--- content/competition.md
+++ content/competition.md
@@ -1,9 +1,15 @@
-## Round 1
+## Round 1 (Individual)
 
-This is some test information about the first round.
+While in their home country, participants will respond to four extended response questions. This work will be submitted for grading and will contribute to students' overall performance in the AAEO.
 
-## Round 2
+The questions will require students to apply elements of economic theory and logic to current economic developments. Students will complete their responses via an online platform while supervised virtually by the AAEO organising committee.
 
-This is some test information about the second round.
+## Round 2 (Team)
 
+Each team representing its own country will be asked to create a presentation that sets out policy options to address an economic challenge in a country from the region. The presentations will be judged on their use of economic theory and logic, the clarity of their reasoning, and the quality of their explanations of how the proposed policy options may increase social welfare and may be evaluated in future against this goal.
 
+All participants will be informed of the policy area serving as the focus for the team round before they depart their home country. This will allow students to undertake some preliminary research before they commence the team round.
+
+## Grading
+
+Students will be graded on both rounds of the competition, with the top 10 performers presented with prizes. The top six students will receive gold, silver, and bronze medals. The next tier of four performers will be given *Honourable Mention* awards.
--- src/templates/index.rs
+++ src/templates/index.rs
@@ -7,7 +7,8 @@ fn index_page<G: Html>(cx: Scope, state: IndexState) -> View<G> {
     view! {
         cx,
         Container(title = "AAEO") {
-            section(id = "welcome", class = "w-full h-screen grid grid-rows-3 md:grid-rows-1 md:grid-cols-3") {
+            // Grid rows are sized on mobile to contain the images
+            section(id = "welcome", class = "w-full h-screen grid grid-rows-[1fr_1fr_20rem] md:grid-rows-1 md:grid-cols-3") {
                 div(class = "flex flex-col justify-center row-span-2 md:col-span-2 text-center md:text-left md:pl-8 lg:pl-16 xl:pl-20 md:px-4 bg-welcome bg-cover") {
                     h1(class = "text-3xl md:text-5xl text-neutral-600 font-extrabold mb-2 md:mb-6 mt-16 md:mt-10 max-w-4xl") { "The Australasian Economics Olympiad" }
                     h3(class = "text-neutral-600 text-xl md:text-3xl font-extrabold") { "Discover the Economist in You" }
@@ -15,7 +16,7 @@ fn index_page<G: Html>(cx: Scope, state: IndexState) -> View<G> {
                     div(class = "font-semibold flex justify-center md:justify-start mt-2") {
                         a(
                             class = "rounded-lg bg-amber-500 hover:bg-amber-400 transition-colors duration-200 text-lg p-2 px-4 text-white mr-2",
-                            href = "mailto:ciee@unsw.edu.au" // TODO Change
+                            href = "mailto:admin@aeo.org.au?subject=An Inquiry about the AAEO" // TODO Change this when the AAEO has an email address
                         ) { "Contact us" }
                         a(
                             class = "rounded-lg border-2 border-neutral-600 hover:bg-amber-400 hover:border-amber-400 transition-colors duration-200 text-lg p-2 px-4 text-neutral-600 hover:text-white",
@@ -25,19 +26,19 @@ fn index_page<G: Html>(cx: Scope, state: IndexState) -> View<G> {
                 }
                 div(class = "h-full w-full") {
                     span(class = "flex justify-center items-center h-full w-full bg-blue-700 text-white") {
-                        p(class = "font-bold text-lg") { "Image here" }
+                        img(class = "object-cover h-full w-full", src = ".perseus/static/people.jpg") {}
                     }
                 }
             }
-            section(id = "competition", class = "w-full min-h-screen grid grid-rows-3 md:grid-rows-1 md:grid-cols-3") {
+            section(id = "competition", class = "w-full min-h-screen grid grid-rows-[1fr_1fr_20rem] md:grid-rows-1 md:grid-cols-3") {
                 div(class = "h-full w-full") {
                      span(class = "flex justify-center items-center h-full w-full bg-blue-700 text-white") {
-                        p(class = "font-bold text-lg") { "Image here" }
+                         img(class = "object-cover h-full w-full", src = ".perseus/static/compass.jpg") {}
                     }
                 }
                 div(class = "row-start-1 row-span-2 md:col-span-2 md:col-start-2 flex flex-col justify-center items-center p-4 py-8 bg-competition bg-cover") {
                     div(
-                        class = "prose prose-slate prose-h1:underline md:text-right md:ml-20",
+                        class = "prose prose-slate prose-h1:underline md:text-right md:ml-20 my-6",
                         dangerously_set_inner_html = &format!("<h1>Competition</h1>{}", state.competition_html),
                     ) {}
                 }
@@ -45,7 +46,7 @@ fn index_page<G: Html>(cx: Scope, state: IndexState) -> View<G> {
             section(id = "about", class = "w-full min-h-screen py-8 flex flex-col justify-center items-center w-full") {
                 div(class = "max-w-4xl text-center") {
                     h1(class = "text-4xl") { "About Us" }
-                    p(class = "mt-1 mb-4") { (state.about) }
+                    p(class = "mt-3 mb-6") { (state.about) }
                     h3(class = "text-2xl mb-2") { "Our Sponsors" }
                     (SPONSOR.widget(cx, "unsw", ()))
                     (SPONSOR.widget(cx, "ciee", ()))
