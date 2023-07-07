d203de9e0c14b4794fbe48e7e0b76012d81810b1
arctic-hen7 <arctic-hen7@users.noreply.github.com>
2023-07-07 22:57:28 +0000

    deploy: a5e9c8831dfd8e1ff7c4929184ac2621bc783f33

---
+++ .perseus/snippets/aaeo-website-819d6a467bb31bdd/src/supabase.js
@@ -0,0 +1,40 @@
+export function create_client(url, anon_key) {
+    window.supabase_client = supabase.createClient(url, anon_key);
+}
+export async function sign_in(email) {
+    const { error } = await window.supabase_client.auth.signInWithOtp({
+        email,
+        options: {
+            emailRedirectTo: "http://localhost:8080/schedule"
+        }
+    });
+    if (error) throw error;
+}
+export async function get_session() {
+    const { data, error } = await window.supabase_client.auth.getSession()
+    if (error) throw error;
+
+    return {
+        email: data.session.user.email,
+        jwt: data.session.access_token,
+    };
+}
+export async function fetch_schedule() {
+    const { data: contents, error: contents_err } = await window.supabase_client
+                                        .from("schedule")
+                                        .select("key, value")
+                                        .eq("key", "contents");
+    if (contents_err) throw contents_err;
+    const { data: banner, error: banner_err } = await window.supabase_client
+                                        .from("schedule")
+                                        .select("key, value")
+                                        .eq("key", "banner");
+    if (banner_err) throw banner_err;
+
+    // If the user was authenticated and authorised, the result we want should be the first (and only, `key` is unique)
+    // row we got back
+    return {
+        banner: banner[0]?.value,
+        contents: contents[0]?.value,
+    };
+}
