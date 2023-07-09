7a0f4ed349f3622f7e898b21542d6e31fe8216ab
arctic-hen7 <arctic.hen@pm.me>
2023-07-10 09:41:58 +1000

    perf: added preloads for images
    This *might* improve load times...

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,21 @@ pub fn main<G: Html>() -> PerseusApp<G> {
                 cx,
                 html(lang = "en") {
                     head {
-                        link(rel = "stylesheet", href = ".perseus/static/tailwind.css") {}
                         meta(name = "viewport", content = "width=device-width") {}
                         meta(name = "description", content = "Information about the Australasian Economics Olympiad (AAEO).")
+
+                        link(rel = "stylesheet", href = ".perseus/static/tailwind.css") {}
                         script(src = "https://unpkg.com/@supabase/supabase-js@2") {}
+
+                        // Image preloads
+                        link(rel = "preload", href = ".perseus/static/logo.ico", as = "image") {}
+                        link(rel = "preload", href = ".perseus/static/welcome_bg.svg", as = "image") {}
+                        link(rel = "preload", href = ".perseus/static/competition_wave.svg", as = "image") {}
+                        link(rel = "preload", href = ".perseus/static/people.avif", as = "image") {}
+                        link(rel = "preload", href = ".perseus/static/compass.avif", as = "image") {}
+                        link(rel = "preload", href = ".perseus/static/ciee.avif", as = "image") {}
+                        link(rel = "preload", href = ".perseus/static/unsw.avif", as = "image") {}
+                        link(rel = "preload", href = ".perseus/static/elite.avif", as = "image") {}
                     }
                     body {
                         PerseusRoot()
